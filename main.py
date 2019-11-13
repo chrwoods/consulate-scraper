a30aedeb03a0c34ae55c632b0b48f3d39aeb8180
Christopher Woods <chrisscottwoods@gmail.com>
2019-11-13 16:45:42 -0600

    Added sending daily alert to ensure service is working

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from scraper import Scraper
 from sms import Texter
+from datetime import datetime
 
 scraper = Scraper()
 scraper.login()
 header = scraper.get_earliest_appointment_header()
 
 if header:
-    texter = Texter()
-    texter.send(header)
+    if '2019' in header:
+        texter = Texter()
+        texter.send('Open appointment found in range: ' + header + '!')
+    # Send a daily alert at 8 am so I know that the service is still running.
+    elif datetime.now().strftime('%H') == '8':
+        texter = Texter()
+        texter.send('DAILY UPDATE: '
+                    'Earliest appointment is in range: ' + header + '.')
